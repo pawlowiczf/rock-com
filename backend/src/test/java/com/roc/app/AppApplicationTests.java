3a6fd6b8470c3e32aef3a7ca3a1436d1a6f6bed6
pawlowiczf <pawlowicz.filip137@gmail.com>
2025-04-02 23:44:36 +0200

    setup backend spring initializer, tests workflow

---
+++ backend/src/test/java/com/roc/app/AppApplicationTests.java
@@ -0,0 +1,21 @@
+package com.roc.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void trueTest() {
+		assertEquals(true, true);
+	}
+}
