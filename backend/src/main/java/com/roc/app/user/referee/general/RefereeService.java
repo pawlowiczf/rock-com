6ab18a4df7dd7a5beadfa43fb0ac3d974aad1a54
Katarzyna Lisiecka <lisiecka.katarzyna.lk@gmail.com>
2025-05-08 09:19:00 +0200

    make Referee extend User

--- backend/src/main/java/com/roc/app/user/referee/general/RefereeService.java
+++ backend/src/main/java/com/roc/app/user/referee/general/RefereeService.java
@@ -1,35 +1,31 @@
 package com.roc.app.user.referee.general;
 
-import com.roc.app.user.general.UserService;
-import com.roc.app.user.general.dto.UserCreateRequestDto;
-import com.roc.app.user.general.dto.UserResponseDto;
-import com.roc.app.user.participant.exception.ParticipantNotFoundException;
+
 import com.roc.app.user.referee.exception.RefereeNotFoundException;
-import com.roc.app.user.general.User;
-import com.roc.app.user.general.UserRepository;
 import com.roc.app.user.referee.general.dto.RefereeCreateRequestDto;
 import com.roc.app.user.referee.general.dto.RefereeResponseDto;
 import jakarta.transaction.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RefereeService {
     private final RefereeRepository refereeRepository;
-    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public RefereeService(RefereeRepository refereeRepository, UserService userService) {
+    public RefereeService(RefereeRepository refereeRepository, PasswordEncoder passwordEncoder) {
         this.refereeRepository = refereeRepository;
-        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public RefereeResponseDto createReferee(RefereeCreateRequestDto requestDto) {
-        UserCreateRequestDto userCreateRequestDto = requestDto.toUserCreateRequestDto();
-        UserResponseDto userResponseDto = userService.createUser(userCreateRequestDto);
-
-        User user = userService.getUserByUserId(userResponseDto.userId());
         Referee referee = Referee.builder()
-                .userDetails(user)
+                .firstName(requestDto.firstName())
+                .lastName(requestDto.lastName())
+                .email(requestDto.email())
+                .password(passwordEncoder.encode(requestDto.password()))
+                .phoneNumber(requestDto.phoneNumber())
                 .build();
 
         refereeRepository.save(referee);
--- backend/src/main/java/com/roc/app/user/referee/general/dto/RefereeCreateRequestDto.java
+++ backend/src/main/java/com/roc/app/user/referee/general/dto/RefereeCreateRequestDto.java
@@ -1,6 +1,5 @@
 package com.roc.app.user.referee.general.dto;
 
-import com.roc.app.user.general.dto.UserCreateRequestDto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -13,11 +12,9 @@ public record RefereeCreateRequestDto(
         @NotBlank @Size(max = 50) @Email
         String email,
         @NotBlank @Size(max = 50)
+        String password,
+        @NotBlank @Size(max = 50)
         String city,
         @NotBlank @Size(max = 12)
         String phoneNumber
-) {
-    public UserCreateRequestDto toUserCreateRequestDto() {
-        return new UserCreateRequestDto(firstName, lastName, email, city, phoneNumber);
-    }
-}
+) {}
