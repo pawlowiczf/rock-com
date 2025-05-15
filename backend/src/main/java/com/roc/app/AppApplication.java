0659705910dde3a4e74a0a8dcc692e1f74105215
pawlowiczf <pawlowicz.filip137@gmail.com>
2025-05-15 09:45:49 +0200

    [AWS] Configured Kubernetes backend deployment. Fixed backend Dockerfile

--- backend/src/main/java/com/roc/app/AppApplication.java
+++ backend/src/main/java/com/roc/app/AppApplication.java
@@ -3,6 +3,26 @@ package com.roc.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+class PrintEnvVariables implements CommandLineRunner {
+
+	@Autowired
+	private Environment env;
+
+	@Override
+	public void run(String... args) {
+		System.out.println("SPRING_PROFILES_ACTIVE: " + env.getProperty("spring.profiles.active"));
+		System.out.println("DATASOURCE URL: " + env.getProperty("spring.datasource.url"));
+		System.out.println("DATASOURCE USERNAME: " + env.getProperty("spring.datasource.username"));
+		System.out.println("DATASOURCE PASSWORD: " + env.getProperty("spring.datasource.password"));
+	}
+}
+
 @SpringBootApplication
 public class AppApplication {
 
