751d9a89f41d814c136941da79a9045617fa33f9
pawlowiczf <pawlowicz.filip137@gmail.com>
2025-05-16 14:32:02 +0200

    [AWS] Kubernetes services deployment

--- frontend/src/pages/UserTypeChoser.tsx
+++ frontend/src/pages/UserTypeChoser.tsx
@@ -68,6 +68,8 @@ const UserTypeChoser: React.FC = () => {
                     birthDate: data.birthdate,
                 };
                 console.log("Participant data:", participantData);
+                console.log(HTTP_ADDRESS)
+                console.log(`${HTTP_ADDRESS}/api/participant`)
                 const response = await fetch(
                     `${HTTP_ADDRESS}/api/participant`,
                     {
