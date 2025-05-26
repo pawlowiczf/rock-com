5e61b272dc6684e0aea8272246ed9791f3f1980a
Bartlomiej Treska <bartlomiej.treska@gmail.com>
2025-05-26 19:27:29 +0200

    Change repository to use corrected primary key

--- backend/src/main/java/com/roc/app/competition/assignment/CompetitionParticipantRepository.java
+++ backend/src/main/java/com/roc/app/competition/assignment/CompetitionParticipantRepository.java
@@ -3,7 +3,7 @@ package com.roc.app.competition.assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface CompetitionParticipantRepository extends JpaRepository<CompetitionParticipant, Integer> {
+public interface CompetitionParticipantRepository extends JpaRepository<CompetitionParticipant, CompetitionParticipant.CompetitionParticipantId> {
     boolean existsCompetitionParticipantByCompetitionIdAndParticipantId(Integer competitionId, Long participantId);
 
     default boolean isEnrolled(Integer competitionId, Long participantId) {
