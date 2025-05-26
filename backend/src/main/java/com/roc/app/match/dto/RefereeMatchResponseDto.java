20a6feb1bb43d71f8851529c4ee799038be4f672
Katarzyna Lisiecka-Meller <51798453+LisieckaK00@users.noreply.github.com>
2025-05-26 19:39:34 +0200

    [ROC-107] Uzupełnić endpointy do dat turniejów o metody do pobierania i aktualizacji (#22)
    * add get,put,delete methods for competition dates
    
    * remove annotations from response dtos

--- backend/src/main/java/com/roc/app/match/dto/RefereeMatchResponseDto.java
+++ backend/src/main/java/com/roc/app/match/dto/RefereeMatchResponseDto.java
@@ -1,24 +1,14 @@
 package com.roc.app.match.dto;
 
 import com.roc.app.match.MatchStatus;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record RefereeMatchResponseDto(
-        @NotNull(message = "Match ID is required")
         Integer matchId,
-
-        @NotNull(message = "Competition ID is required")
         Integer competitionId,
-
-        @NotNull(message = "Match date is required")
         LocalDateTime matchDate,
-
-        @NotBlank(message = "Status is required")
         MatchStatus status,
-
         String player1Name,
         String player2Name,
         String score
