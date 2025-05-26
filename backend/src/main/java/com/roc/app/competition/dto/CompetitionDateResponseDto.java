27944b8e10746f5f10b6f8bf2476449ba62e948e
Katarzyna Lisiecka-Meller <lisiecka.katarzyna.lk@gmail.com>
2025-05-26 19:29:22 +0200

    remove annotations from response dtos

--- backend/src/main/java/com/roc/app/competition/dto/CompetitionDateResponseDto.java
+++ backend/src/main/java/com/roc/app/competition/dto/CompetitionDateResponseDto.java
@@ -1,18 +1,13 @@
 package com.roc.app.competition.dto;
 
 import com.roc.app.competition.CompetitionDate;
-import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record CompetitionDateResponseDto (
-        @NotNull(message = "Competition date id is required")
         Integer dateId,
-        @NotNull(message = "Competition id is required")
         Integer competitionId,
-        @NotNull(message = "Start time is required")
         LocalDateTime startTime,
-        @NotNull(message = "End time is required")
         LocalDateTime endTime
 ){
     public static CompetitionDateResponseDto fromModel(CompetitionDate competitionDate) {
